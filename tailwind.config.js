9b47a782509c4df23057d438b66361ec01c7e83e
michel <michel.cavaud@gmail.com>
2021-04-23 17:53:44 +0200

    Ajout couleurs et font à config Tailwincss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+       'croissant': ['Croissant One', 'cursive']
+    },
+    colors: {
+      noir: '#000',
+      blanc: '#fff',
+      fond: "#F9D195",
+      cyanclair: '#99E3EC',
+      cyanfonce: '#6BBFC8' 
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
